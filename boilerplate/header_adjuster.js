5b098fdba14cdff36e55abca52ccdc58d66841aa
yumetodo <yume-wikijp@live.jp>
2017-02-02 06:45:29 +0900

    simplify code and add JSDoc comment

--- boilerplate/header_adjuster.js
+++ boilerplate/header_adjuster.js
@@ -2,25 +2,32 @@
     'use strict';
 
     /**
-     * @param propatyValue {string}
+     * @brief get propaty value as string, remove'px' and parseInt
+     * @param dec {CSSStyleDeclaration}
+     * @param propatyName {string}
      */
-    const extractPx = function(propatyValue) {
-        return parseInt(propatyValue.replace(/([0-9]+)px/, '$1'), 10);
+    const extractPx = function(dec, propatyName) {
+        return parseInt(dec.getPropertyValue(propatyName).replace(/([0-9]+)px/, '$1'), 10);
     };
     /**
+     * @brief convert to string and append 'px'
      * @param n {number}
      */
     const setPx = function(n) {
         return (0 === n) ? '0' : n + 'px';
     };
+    /**
+     * @brief adjust margin and padding of #blog-title
+     */
     const adjustHeader = function() {
         const ratio = 0.20;
         const minMarginBottom = 3;
 
         const header = document.getElementById('blog-title');
-        const headerMarginTop = extractPx(window.getComputedStyle(header).getPropertyValue('margin-top'));
-        const headerMarginBottom = extractPx(window.getComputedStyle(header).getPropertyValue('margin-bottom'));
-        const currentTotalHeaderHeight = header.offsetHeight + headerMarginTop + headerMarginBottom;
+        const computedHeader = window.getComputedStyle(header);
+        const currentTotalHeaderHeight = header.offsetHeight
+                                       + extractPx(computedHeader, 'margin-top')
+                                       + extractPx(computedHeader, 'margin-bottom');
         const expectTotalHeaderHeight = ratio * document.body.offsetWidth;
         if (currentTotalHeaderHeight != expectTotalHeaderHeight) {
             const margin2x = expectTotalHeaderHeight - header.offsetHeight;
@@ -28,7 +35,7 @@
             header.style.marginTop = setPx((margin < minMarginBottom) ? Math.max(margin2x, 0) : margin);
             header.style.marginBottom = setPx((margin < minMarginBottom) ? minMarginBottom : margin);
             if (margin < minMarginBottom) {
-                const headerPureHeight = extractPx(window.getComputedStyle(header).getPropertyValue('height'));
+                const headerPureHeight = extractPx(computedHeader, 'height');
                 const padding = Math.max(expectTotalHeaderHeight - headerPureHeight, 0);
                 header.style.paddingTop = setPx(padding / 2);
                 header.style.paddingBottom = setPx(padding / 2);
