7e0c6e366ff9c3e739e2f5237f2926c7afc5f37a
yumetodo <yume-wikijp@live.jp>
2017-02-03 23:39:35 +0900

    stop creating closure-compiler-vxxx.jar
    duplicated

--- closure-compiler-install.js
+++ closure-compiler-install.js
@@ -4,7 +4,7 @@ const fsp = require('fs-promise');
 const download = require('download');
 const path = require('path');
 console.log('installing Google Closure Compiler...');
-download('http://dl.google.com/closure-compiler/compiler-latest.zip', './', {
+download('http://dl.google.com/closure-compiler/compiler-latest.zip', {
     extract: true,
     filter:  file => path.extname(file.path) === '.jar',
 }).then(d => fsp.writeFile('closure-compiler.jar', d[0].data)).then(() => console.log('done.'));
