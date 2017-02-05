7016eb28d6c681618991368d6849d607558dacdb
yumetodo <yume-wikijp@live.jp>
2017-02-05 12:41:43 +0900

    change deploy key

--- tools/shippable_deploy.sh
+++ tools/shippable_deploy.sh
@@ -1,11 +1,11 @@
 echo "ll /tmp/ssh"
 ls -l /tmp/ssh
-echo "ll /tmp/ssh/GithubDeploy"
+echo "ll /tmp/ssh/shippable2githubssh"
 ls -l /tmp/ssh
 echo "ll /dev/shm"
 ls -l /dev/shm
 echo "clone taget repogitry..."
-ssh-agent bash -c 'ssh-add /tmp/ssh/GithubDeploy; git clone -b gh-pages git@github.com:yumetodo/Hatena-Blog-Themes.git /dev/shm/Hatena-Blog-Themes'
+ssh-agent bash -c 'ssh-add /tmp/ssh/shippable2githubssh; git clone -b gh-pages git@github.com:yumetodo/Hatena-Blog-Themes.git /dev/shm/Hatena-Blog-Themes'
 bash -c 'cd /dev/shm/Hatena-Blog-Themes; git status;'
 cd $SHIPPABLE_BUILD_DIR
 echo "copy minified result..."
@@ -18,6 +18,6 @@ git add .
 echo "commit changes..."
 git commit -m "Deploy minified css/js [skip ci]"
 echo "push commit..."
-ssh-agent bash -c 'ssh-add /tmp/ssh/GithubDeploy; git push'
+ssh-agent bash -c 'ssh-add /tmp/ssh/shippable2githubssh; git push'
 echo "finish deploy."
 cd $SHIPPABLE_BUILD_DIR
