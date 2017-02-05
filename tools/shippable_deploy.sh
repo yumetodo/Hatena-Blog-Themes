9e7b0cba73de2300da58423da0388bc3670227f4
yumetodo <yume-wikijp@live.jp>
2017-02-05 11:59:08 +0900

    Display more information

--- tools/shippable_deploy.sh
+++ tools/shippable_deploy.sh
@@ -1,13 +1,21 @@
-ls -l /tmp/ssh/
+echo "ll /tmp/ssh"
+ls -l /tmp/ssh
+echo "ll /dev/shm"
+ls -l /dev/shm
+echo "clone taget repogitry..."
 ssh-agent bash -c 'ssh-add /tmp/ssh/githubdeploy; git clone -b gh-pages git@github.com:yumetodo/Hatena-Blog-Themes.git /dev/shm/Hatena-Blog-Themes'
 bash -c 'cd /dev/shm/Hatena-Blog-Themes; git status;'
 cd $SHIPPABLE_BUILD_DIR
+echo "copy minified result..."
 cp -r bin /dev/shm/Hatena-Blog-Themes
 cd /dev/shm/Hatena-Blog-Themes
 git config user.name "yumetodo"
 git config user.email "yume-wikijp@live.jp"
 git config push.default simple
 git add .
+echo "commit changes..."
 git commit -m "Deploy minified css/js [skip ci]"
+echo "push commit..."
 ssh-agent bash -c 'ssh-add /tmp/ssh/githubdeploy; git push'
+echo "finish deploy."
 cd $SHIPPABLE_BUILD_DIR
