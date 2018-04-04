0a4fe59d130b21d1121198b0213730730c0f8548
yumetodo <yume-wikijp@live.jp>
2018-04-04 17:41:57 +0900

    update npm dependency

--- closure-compiler-install.js
+++ closure-compiler-install.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const fsp = require('fs-promise');
+const fse = require('fs-extra');
 const download = require('download');
 const path = require('path');
 console.log('installing Google Closure Compiler...');
 download('http://dl.google.com/closure-compiler/compiler-latest.zip', {
     extract: true,
     filter:  file => path.extname(file.path) === '.jar',
-}).then(d => fsp.writeFile('closure-compiler.jar', d[0].data)).then(() => console.log('done.'));
+}).then(d => fse.writeFile('closure-compiler.jar', d[0].data)).then(() => console.log('done.'));
